9701f25e4883a73e13f8adf979ade748004e1e2e
ahmer7aa <ahmer5253@gmail.com>
2021-08-12 13:10:22 +0500

    Final Touch

--- lib/screens/home/coffeeTile.dart
+++ lib/screens/home/coffeeTile.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:coffee_app/models/brew.dart';
 
@@ -15,6 +16,18 @@ class BrewTile extends StatelessWidget {
             radius: 25.0,
             backgroundColor: Colors.brown[int.parse(brew.strength)],
             backgroundImage: AssetImage('images/coffee_icon.png'),
+          ),
+          trailing: Padding(
+            padding: EdgeInsets.only(top: 40.0),
+            child: Text(
+              "Strength: " + brew.strength,
+              textAlign: TextAlign.end,
+              style: TextStyle(
+                fontSize: 12.0,
+                color: Colors.brown,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
           ),
           title: Text(brew.name),
           subtitle: Text('Takes ${brew.sugars} sugar(s)'),
